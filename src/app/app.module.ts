1b4314d65c8397993375907653f98b9d53186b46
ChikaEche <chikaokafor94@gmail.com>
2021-08-31 13:34:55 +0100

    components wip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,25 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
 import { AppRoutingModule } from './app-routing.module';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getAuth, provideAuth } from "@angular/fire/auth";
+import { environment } from 'src/environments/environment';
+import { LayoutComponent } from './layout/layout.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    LoginComponent,
+    DashboardComponent
   ],
   imports: [
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideFirestore(() => getFirestore()),
+    provideAuth(() => getAuth()),
     BrowserModule,
     BrowserAnimationsModule,
     NbThemeModule.forRoot({ name: 'dark' }),
